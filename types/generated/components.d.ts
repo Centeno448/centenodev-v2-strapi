01f889e77e4ea89c4345f0b8d128b02748638b2c
Centeno448 <diegocenteno448@gmail.com>
2025-01-07 16:04:53 -0500

    update components

---
+++ types/generated/components.d.ts
@@ -0,0 +1,87 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
+export interface SharedText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_texts';
+  info: {
+    displayName: 'Text';
+    icon: 'pencil';
+  };
+  attributes: {
+    Text: Schema.Attribute.Text;
+  };
+}
+
+export interface SharedTextList extends Struct.ComponentSchema {
+  collectionName: 'components_shared_text_lists';
+  info: {
+    description: '';
+    displayName: 'Text List';
+    icon: 'bulletList';
+  };
+  attributes: {
+    item: Schema.Attribute.Component<'shared.text', true>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.media': SharedMedia;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
+      'shared.text': SharedText;
+      'shared.text-list': SharedTextList;
+    }
+  }
+}
